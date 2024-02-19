4c920898f753ddaae31b23a67c347faa1c117050
Bool <bool.rus@gmail.com>
2024-02-19 13:58:17 +0300

    able to edit/delete messages

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,85 @@
 use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
+use std::sync::{Arc, Mutex};
 
 use clap::Parser;
 use serde::{Deserialize, Serialize};
-use teloxide::Bot;
-use teloxide::types::Message;
+use teloxide::dispatching::Dispatcher;
+use teloxide::{dptree, Bot};
 
-#[ctor::ctor]
-static CONF: Config = Config::parse();
+
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
+    let conf = Config::parse();
+    let conf = Arc::new(conf);
+    let data = load_data()?;
+    let data = Arc::new(Mutex::new(data));
+    let bot = Bot::new(conf.token.clone());
 
-    let bot = Bot::new(CONF.token.clone());
+    let handler = dptree::entry()
+        .endpoint(process_update);
+    Dispatcher::builder(bot.clone(), handler)
+        .dependencies(dptree::deps![conf, data])
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch().await;
+    Ok(())
+}
 
+fn load_data() -> anyhow::Result<Data> {
+    let file = File::open("data.json")?;
+    let reader = BufReader::new(file);
+    serde_json::from_reader(reader).map_err(Into::into)
+}
 
-    teloxide::repl(bot, |msg: Message| async move {
-        let from_id = msg.from().map(|u|u.id.0).unwrap_or_default();
-        if !CONF.admins.contains(&from_id) {
-            return Ok(())
-        }
-        if let Some(text) = msg.text() {
-            let b = Bulletin {
+fn store_data(data: &Data) -> anyhow::Result<()> {
+    let file = File::create("data.json")?;
+    let writer = BufWriter::new(file);
+    serde_json::to_writer(writer, data).map_err(Into::into)
+}
+
+async fn process_update(
+        conf: Arc<Config>, 
+        data: Arc<Mutex<Data>>, 
+        upd: teloxide::types::Update
+    ) -> anyhow::Result<()> {
+    use teloxide::types::UpdateKind::*;
+    let (msg, edit) = match upd.kind {
+        Message(msg) | ChannelPost(msg) => (msg, false),
+        EditedMessage(msg) | EditedChannelPost(msg) => (msg, true),
+        _ => return Ok(())
+    };
+    let from_id = msg.from().map(|u|u.id.0).unwrap_or_default();
+    if !conf.admins.contains(&from_id) {
+        return Ok(())
+    }
+    if let Some(text) = msg.text() {
+        let id = (msg.id.0 as u64) * from_id;
+        let mut data = data.lock().unwrap();
+        if edit {
+            if text == "del" {
+                data.bulletins.retain(|b|b.id != id);
+            } else {
+                if let Some(b) = data.bulletins.iter_mut().find(|b|b.id == id) {
+                    b.text = text.to_owned();
+                }
+            }
+        } else {
+            data.bulletins.push_front(Bulletin {
                 id: (msg.id.0 as u64) * from_id,
                 ts: msg.date.timestamp(),
                 important: false, 
                 text: text.to_owned(),
-            };
-            let file = File::open("data.json")?;
-            let reader = BufReader::new(file);
-            let mut data: Data = serde_json::from_reader(reader).unwrap();
-            data.bulletins.push_front(b);
-            let file = File::create("data.json")?;
-            let writer = BufWriter::new(file);
-            serde_json::to_writer(writer, &data).unwrap();
+            });
         }
-        Ok(())
-    }).await;
+        store_data(&data)?;
+    }
+    Ok(())
 }
+
+
 
 #[derive(Serialize, Deserialize)]
 struct Bulletin {
